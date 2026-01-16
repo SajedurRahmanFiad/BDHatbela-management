0a5a9e249b2ea94b0a422baea9f62d79b1439d7d
Sajedur Rahman Fiad <sajedurrahmanfiad@gmail.com>
2026-01-16 19:41:49 +0600

    Added carrybee in settings

--- app/Http/Controllers/Settings/Courier.php
+++ app/Http/Controllers/Settings/Courier.php
@@ -14,13 +14,44 @@ class Courier extends SettingController
 
     public function update(Request $request)
     {
-        // Call parent update method
-        $response = parent::update($request);
+        $fields = $request->all();
+
+        // Handle Steadfast settings
+        if (isset($fields['steadfast_base_url'])) {
+            setting()->set('courier.steadfast.base_url', $fields['steadfast_base_url']);
+        }
+        if (isset($fields['steadfast_api_key'])) {
+            setting()->set('courier.steadfast.api_key', $fields['steadfast_api_key']);
+        }
+        if (isset($fields['steadfast_secret_key'])) {
+            setting()->set('courier.steadfast.secret_key', $fields['steadfast_secret_key']);
+        }
+
+        // Handle CarryBee settings
+        if (isset($fields['carrybee_base_url'])) {
+            setting()->set('courier.carrybee.base_url', $fields['carrybee_base_url']);
+        }
+        if (isset($fields['carrybee_client_id'])) {
+            setting()->set('courier.carrybee.client_id', $fields['carrybee_client_id']);
+        }
+        if (isset($fields['carrybee_client_secret'])) {
+            setting()->set('courier.carrybee.client_secret', $fields['carrybee_client_secret']);
+        }
+        if (isset($fields['carrybee_client_context'])) {
+            setting()->set('courier.carrybee.client_context', $fields['carrybee_client_context']);
+        }
+
+        // Save all settings
+        setting()->save();
 
         // Update .env file with the new settings
         $this->updateEnvFile();
 
-        return $response;
+        $message = trans('messages.success.updated', ['type' => trans_choice('general.settings', 2)]);
+
+        flash($message)->success();
+
+        return redirect()->back();
     }
 
     private function updateEnvFile()
@@ -30,6 +61,11 @@ class Courier extends SettingController
         $apiKey = setting('courier.steadfast.api_key') ?? '';
         $secretKey = setting('courier.steadfast.secret_key') ?? '';
 
+        $carrybeeBaseUrl = setting('courier.carrybee.base_url') ?? '';
+        $carrybeeClientId = setting('courier.carrybee.client_id') ?? '';
+        $carrybeeClientSecret = setting('courier.carrybee.client_secret') ?? '';
+        $carrybeeClientContext = setting('courier.carrybee.client_context') ?? '';
+
         // Read current .env file
         $envFile = base_path('.env');
         $envContent = file_get_contents($envFile);
@@ -38,6 +74,11 @@ class Courier extends SettingController
         $envContent = preg_replace('/STEADFAST_BASE_URL=.*/', 'STEADFAST_BASE_URL="' . $baseUrl . '"', $envContent);
         $envContent = preg_replace('/STEADFAST_API_KEY=.*/', 'STEADFAST_API_KEY="' . $apiKey . '"', $envContent);
         $envContent = preg_replace('/STEADFAST_SECRET_KEY=.*/', 'STEADFAST_SECRET_KEY="' . $secretKey . '"', $envContent);
+
+        $envContent = preg_replace('/CARRYBEE_BASE_URL=.*/', 'CARRYBEE_BASE_URL="' . $carrybeeBaseUrl . '"', $envContent);
+        $envContent = preg_replace('/CARRYBEE_CLIENT_ID=.*/', 'CARRYBEE_CLIENT_ID="' . $carrybeeClientId . '"', $envContent);
+        $envContent = preg_replace('/CARRYBEE_CLIENT_SECRET=.*/', 'CARRYBEE_CLIENT_SECRET="' . $carrybeeClientSecret . '"', $envContent);
+        $envContent = preg_replace('/CARRYBEE_CLIENT_CONTEXT=.*/', 'CARRYBEE_CLIENT_CONTEXT="' . $carrybeeClientContext . '"', $envContent);
 
         // Write back to .env
         file_put_contents($envFile, $envContent);
--- resources/views/settings/courier/edit.blade.php
+++ resources/views/settings/courier/edit.blade.php
@@ -10,11 +10,27 @@
                     </x-slot>
 
                     <x-slot name="body">
-                        <x-form.group.text name="base_url" label="Base URL" value="{{ setting('courier.steadfast.base_url') }}" not-required />
+                        <x-form.group.text name="steadfast_base_url" label="Base URL" value="{{ setting('courier.steadfast.base_url') }}" not-required />
 
-                        <x-form.group.text name="api_key" label="API Key" value="{{ setting('courier.steadfast.api_key') }}" not-required />
+                        <x-form.group.text name="steadfast_api_key" label="API Key" value="{{ setting('courier.steadfast.api_key') }}" not-required />
 
-                        <x-form.group.text name="secret_key" label="Secret Key" value="{{ setting('courier.steadfast.secret_key') }}" not-required />
+                        <x-form.group.text name="steadfast_secret_key" label="Secret Key" value="{{ setting('courier.steadfast.secret_key') }}" not-required />
+                    </x-slot>
+                </x-form.section>
+
+                <x-form.section>
+                    <x-slot name="head">
+                        <x-form.section.head title="CarryBee" description="Configure CarryBee courier integration settings" />
+                    </x-slot>
+
+                    <x-slot name="body">
+                        <x-form.group.text name="carrybee_base_url" label="Base URL" value="{{ setting('courier.carrybee.base_url') }}" not-required />
+
+                        <x-form.group.text name="carrybee_client_id" label="Client ID" value="{{ setting('courier.carrybee.client_id') }}" not-required />
+
+                        <x-form.group.text name="carrybee_client_secret" label="Client Secret" value="{{ setting('courier.carrybee.client_secret') }}" not-required />
+
+                        <x-form.group.text name="carrybee_client_context" label="Client Context" value="{{ setting('courier.carrybee.client_context') }}" not-required />
                     </x-slot>
                 </x-form.section>
 
@@ -26,7 +42,7 @@
                 </x-form.section>
                 @endcan
 
-                <x-form.input.hidden name="_prefix" value="courier.steadfast" />
+                <x-form.input.hidden name="_prefix" value="courier" />
             </x-form>
         </x-form.container>
     </x-slot>
